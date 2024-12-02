5ce22213e189d55dd36ab233c2cc4a8af4a62676
slmaxbot <nipundhanujaya01@gmail.com>
2024-12-02 22:41:18 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94729007640"
+const SESSION_ID = "PRABATH-MD~vHAVVYCa#KhX5fCxTPLjvc41dMl2hZXj8xnDPI0R_j2tElullS10"
 
 
 module.exports = {
